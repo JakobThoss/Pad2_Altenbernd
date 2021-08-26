1972ab19a56c2085cc1118d00cdb312ab8e6357a
Jakob Thoss <jakobthoss@Jakobs-MacBook-Pro.local>
2021-08-26 09:14:55 +0200

    Kap_4_3

---
+++ Vorlesung/Kap_4_3/Rational_priorityQueue/main.cpp
@@ -0,0 +1,39 @@
+#include "rational.h"
+#include <queue>
+
+
+
+int main()
+{
+
+    priority_queue<Rational> pq;
+    Rational test(6,4);
+    Rational test2(2,4);
+    Rational ergebnis(4,4);
+
+    pq.push(test);
+    pq.push(test2);
+    pq.push(ergebnis);
+
+    while (!pq.empty()){
+        Rational temp;
+        temp = pq.top();
+        temp.ausgabe();
+        pq.pop();
+    }
+
+
+    bool wert = (test < test2);
+    cout << boolalpha << wert << endl;
+
+
+    ergebnis  = test + test2;
+    cout << " + = ";
+    ergebnis.ausgabe();
+
+    ergebnis = test * test2;
+    cout << " * = ";
+    ergebnis.ausgabe();
+
+    return 0;
+}
---
+++ Vorlesung/Kap_4_3/Rational_priorityQueue/rational.h
@@ -0,0 +1,73 @@
+#ifndef RATIONAL_H
+#define RATIONAL_H
+#include <iostream>
+
+
+using namespace std;
+
+
+
+class Rational
+{
+private:
+    int zaehler;
+    unsigned int nenner;
+public:
+    Rational() {
+        zaehler = 0;
+        nenner = 1;
+    }
+
+    Rational(int p_zaehler, unsigned int p_nenner) {
+        zaehler = p_zaehler;
+        nenner = p_nenner;
+    }
+
+    Rational(long l) {
+        zaehler = l;
+        nenner = 1;
+    }
+    int getZaehler() const {
+        return zaehler;
+    }
+    unsigned int getNenner() const {
+        return nenner;
+    }
+
+    bool operator<(const Rational& b) const {
+        double wert = (double) this->getZaehler() / this->getNenner();
+        double wert2 = (double) b.getZaehler() / b.getNenner();
+        return (wert < wert2);
+    }
+    friend Rational operator+(const Rational & a, const Rational & b) {
+        Rational temp;
+        temp.zaehler = a.zaehler + b.zaehler;
+        temp.nenner = a.nenner;
+        return temp;
+    }
+    friend Rational operator*(const Rational & a, const Rational & b) {
+        Rational temp;
+        temp.zaehler = a.zaehler * b.zaehler;
+        temp.nenner = a.nenner * b.nenner;
+        return temp;
+    }
+
+    void ausgabe(){
+        cout << this->zaehler << "/" << this->nenner << endl;
+    }
+};
+
+bool operator==(const Rational& a, const Rational& b) {
+    return a.getZaehler() == b.getZaehler() && a.getNenner() == b.getNenner();
+}
+
+bool operator==(const Rational& a, int b) {
+    double wert = (double) a.getZaehler() / a.getNenner();
+    return (wert == b);
+}
+
+
+
+
+
+#endif // RATIONAL_H
